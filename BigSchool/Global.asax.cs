b01f6390ff81f3cc733c451de9178e665f9ec327
msin <msin@MSI>
2023-03-16 10:02:42 +0700

    Trang 40

--- BigSchool/Global.asax.cs
+++ BigSchool/Global.asax.cs
@@ -1,12 +1,15 @@
+using BigSchool.App_Start;
 using BigSchool.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace BigSchool
 {
@@ -14,7 +17,8 @@ namespace BigSchool
     {
         protected void Application_Start()
         {
-            Database.SetInitializer<ApplicationDbContext>(null);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            Database.SetInitializer<Models.ApplicationDbContext>(null);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
